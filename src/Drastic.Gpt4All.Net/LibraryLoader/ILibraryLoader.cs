2529d34aa79b931fdee2303f59bce5ae790b97b7
Tim Miller <innerlogic4321@ghmail.com>
2023-05-31 15:52:18 +0900

    Remove legacy code

--- src/Drastic.Gpt4All.Net/LibraryLoader/ILibraryLoader.cs
+++
@@ -1,8 +0,0 @@
-// Licensed under the MIT license: https://opensource.org/licenses/MIT
-
-namespace Gpt4All.LibraryLoader;
-
-public interface ILibraryLoader
-{
-    LoadResult OpenLibrary(string? fileName);
-}
